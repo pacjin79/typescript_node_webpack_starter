f0c84ca55d28e656998154bf18b1f4112166f10b
leojin <pacjin@gmail.com>
2016-10-19 21:57:36 -0400

    added ext resolver

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const fs = require('fs');
+const srcPath = './src';
 
 var nodeModules = {};
 fs.readdirSync('node_modules')
@@ -13,6 +14,11 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'build'),
         filename: 'server.js'
+    },
+    resolve: {
+        root: srcPath,
+        moduleDirectories: ['node_modules'],
+        extensions: ['', '.js', '.ts', '.tsx', '.css']
     },
     externals: nodeModules,
     module: {
